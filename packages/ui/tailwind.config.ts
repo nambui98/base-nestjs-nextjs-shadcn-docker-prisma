1d53938c2375d827784685111b0a8e053ee5a6ca
nambui98 <bvnam98@gmail.com>
2024-10-30 12:45:41 +0700

    first commit

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@repo/tailwind-config";
+
+const config: Pick<Config, "prefix" | "presets" | "content"> = {
+  content: [
+    "../../packages/ui/src/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix: "",
+  presets: [sharedConfig],
+};
+
+export default config;
